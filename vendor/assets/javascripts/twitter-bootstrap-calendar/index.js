c6bc02aa8e3326c99067d32f7413cd4b138ae358
sbauch <sbauch@gmail.com>
2013-03-05 16:20:55 -0500

    Update index.js

--- vendor/assets/javascripts/twitter-bootstrap-calendar/index.js
+++ vendor/assets/javascripts/twitter-bootstrap-calendar/index.js
@@ -1,19 +1,19 @@
 function tbsCalendarSetMaxHeight(){
-  var maxHeight = -1;
+  var maxHeight = 200;
 
-  $('.calendar_grid .week .span1').each(function() {
-    maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
-  });
+//  $('.calendar_grid .week .span1').each(function() {
+//    maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
+//  });
 
   $('.calendar_grid .week .span1').each(function() {
     $(this).height(maxHeight);
   });
 }
 
-//$(document).on('page:load', function() {
-//  tbsCalendarSetMaxHeight();
-//});
+$(document).on('page:load', function() {
+  tbsCalendarSetMaxHeight();
+});
 
-//$(document).ready(function() {
-//  tbsCalendarSetMaxHeight();
-//});
+$(document).ready(function() {
+  tbsCalendarSetMaxHeight();
+});
